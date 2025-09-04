101c3a907b23ac92c6e5c88b903c267d1646d989
dev-t0ny <antoineboudreau@Outlook.com>
2025-09-04 12:10:00 -0400

    devops - ajout d'un alias pour deployer

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tp1-veille/',
   plugins: [
     vue(),
     vueDevTools(),
