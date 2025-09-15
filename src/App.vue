48e43346a9070157f9b7dd7a4f91c296d8196d99
dev-t0ny <antoineboudreau@Outlook.com>
2025-09-15 12:31:05 -0400

    feat - ajout de la deuxième section

--- src/App.vue
+++ src/App.vue
@@ -6,29 +6,54 @@ const afficherDetails = ref(false);
 </script>
 
 <template>
-  <header id="accueil">
-    <nav
-      class="flex justify-center mx-auto gap-8 text-white p-4 mt-5 mb-8 w-1/4"
-    >
-      <ul>
-        <a href="#accueil" class="hover:underline">Accueil</a>
-      </ul>
-        <ul>
-        <a href="#usage" class="hover:underline">Usage</a>
-      </ul>
-      <ul>
-        <a href="#details" class="hover:underline">Détails</a>
-      </ul>
-      <ul>
-        <a href="#experimentation" class="hover:underline">Expérimentations</a>
-      </ul>
-      <ul>
-        <a href="#avis" class="hover:underline">Avis</a>
-      </ul>
-    </nav>
-  </header>
+<header class="fixed top-0 left-0 w-full z-50 backdrop-blur-md bg-[#282828] shadow-lg">
+  <nav class="flex justify-center items-center px-8 py-4">
+    <ul class="flex gap-8 text-white font-medium">
+      <li>
+        <a 
+          href="#accueil" 
+          class="relative px-4 py-2 rounded-lg transition-all duration-300 hover:bg-emerald-100/20 group"
+        >
+          Accueil
+        </a>
+      </li>
+      <li>
+        <a 
+          href="#usage" 
+          class="relative px-4 py-2 rounded-lg transition-all duration-300 hover:bg-emerald-100/20 group"
+        >
+          Usage
+        </a>
+      </li>
+      <li>
+        <a 
+          href="#description" 
+          class="relative px-4 py-2 rounded-lg transition-all duration-300 hover:bg-emerald-100/20 group"
+        >
+          Détails
+        </a>
+      </li>
+      <li>
+        <a 
+          href="#experimentation" 
+          class="relative px-4 py-2 rounded-lg transition-all duration-300 hover:bg-emerald-100/20 group"
+        >
+          Expérimentations
+        </a>
+      </li>
+      <li>
+        <a 
+          href="#avis" 
+          class="relative px-4 py-2 rounded-lg transition-all duration-300 hover:bg-emerald-100/20 group"
+        >
+          Avis
+        </a>
+      </li>
+    </ul>
+  </nav>
+</header>
 
-  <main class="container mx-auto p-4">
+  <main class="container mx-auto p-4" id="accueil">
     <section class="text-center text-white mb-16 mt-50">
       <h1 class="text-4xl font-light mb-2">TP1 - Veille technologique</h1>
       <h2 class="text-2xl text-gray-400">
@@ -42,10 +67,13 @@ const afficherDetails = ref(false);
       </a>
     </section>
 
-    <section id="usage" class="text-white text-center mb-[25rem] mt-[20rem]">
+    <section
+      id="usage"
+      class="text-white text-center mb-[25rem] pt-[10rem] mt-[40rem]"
+    >
       <SplitText
         text="L'appareil que j'ai choisi?"
-        class-name="text-2xl font-semibold text-center"
+        class-name="text-4xl font-semibold text-center"
         :delay="30"
         :duration="0.3"
         ease="power3.out"
@@ -58,9 +86,11 @@ const afficherDetails = ref(false);
         @animation-complete="handleAnimationComplete"
       />
 
-      <div class="flex justify-center items-center gap-12 mt-20">
+      <div
+        class="flex justify-center mx-auto items-center mt-[5rem] gap-12 bg-[#232323] p-10 rounded w-4xl"
+      >
         <div
-          class="w-1/4 p-10 border-4 border-dashed border-gray-600 rounded-lg hover:bg-gray-800 hover:scale-110 transform transition duration-300 ease-in-out relative"
+          class="w-1/4 p-10 border-4 border-dashed border-gray-600 rounded-lg hover:bg-[#494949] hover:scale-110 transform transition duration-300 ease-in-out relative"
           @mouseenter="afficherDetails = true"
         >
           <p v-if="!afficherDetails">Survole moi!</p>
@@ -76,7 +106,7 @@ const afficherDetails = ref(false);
                 alt="Image de l'appareil"
                 class="mb-2 rounded"
               />
-              <h2 class="text-xl">Détecteur de mouvement</h2>
+              <h2 class="text-md">Capteur infrarouge passif</h2>
             </div>
           </transition>
         </div>
@@ -90,12 +120,110 @@ const afficherDetails = ref(false);
           "
           v-show="true"
         >
-          <h3 class="text-lg font-semibold mb-2">Usage proposé</h3>
-          <ul class="list-disc pl-5 text-gray-300">
+          <h3 class="text-lg font-semibold mb-2 underline underline-offset-3">
+            Usage proposé
+          </h3>
+          <ul class="list-none text-gray-300">
             <li>
               Calculer la vitesse d'une personne ou d'un objet en mouvement.
             </li>
+            <li>
+              <img src="/img/animation.gif" class="mt-5" />
+            </li>
           </ul>
+        </div>
+      </div>
+    </section>
+
+    <section
+      id="description"
+      class="text-white text-center mb-[25rem] pt-[10rem] mt-[5rem]"
+    >
+      <h2 class="text-2xl font-semibold mb-16 underline underline-offset-4">
+        Description de l'objet expérimenté
+      </h2>
+
+      <div class="grid grid-cols-3 gap-8 justify-items-center">
+        <div class="bg-[#232323] p-6 rounded-lg w-full max-w-md text-left">
+          <h3
+            class="text-md font-semibold mb-2 bg-[#353535] p-3 text-center rounded"
+          >
+            Résumé des fonctionnalités
+          </h3>
+          <blockquote
+            class="p-4 my-4 border-s-4 border-green-500 bg-green-900 opacity-80"
+          >
+            <p class="text-md italic font-medium leading-relaxed text-white">
+              "Un détecteur infrarouge est un détecteur réagissant à un
+              rayonnement infrarouge"
+
+              <a
+                class="underline block mt-3"
+                href="https://fr.wikipedia.org/wiki/D%C3%A9tecteur_infrarouge"
+                >Wikipedia</a
+              >
+            </p>
+          </blockquote>
+          <p class="text-gray-300 mt-10 text-justify">
+            Le capteur infrarouge permet de capter la présence d’un objet. Il
+            lance un signal électrique lorsqu'il le détecte. À cause de cela, la
+            détection de la vitesse n'est pas supporté de base. C'est en
+            combinant plusieurs de ces appareils
+          </p>
+        </div>
+
+        <div class="bg-[#232323] p-6 rounded-lg w-full max-w-md text-left">
+          <h3
+            class="text-md font-semibold mb-2 bg-[#353535] p-3 text-center rounded"
+          >
+            Informations supplémentaires
+          </h3>
+
+          <h2 class="text-lg font-semibold mb-2 mt-10">Fabricant:</h2>
+          <ul class="list-disc list-inside text-gray-300">
+            <li>Arduino</li>
+          </ul>
+
+          <h2 class="text-lg font-semibold mb-2 mt-15">
+            Autres éléments pertinents:
+          </h2>
+          <ul class="list-disc list-inside text-gray-300">
+            <li>Type de capteur utilisé (ultrason, infrarouge, etc.)</li>
+            <li>Plage de détection (ex. 0–5 m)</li>
+            <li>Précision</li>
+          </ul>
+        </div>
+
+        <div class="bg-[#232323] p-6 rounded-lg w-full max-w-md text-left">
+          <h3
+            class="text-md font-semibold mb-2 bg-[#353535] p-3 text-center rounded"
+          >
+            Images
+          </h3>
+          <figure class="max-w-lg mt-5">
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="/img/schema.png"
+              alt="image description"
+            />
+            <figcaption
+              class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400"
+            >
+              Schéma du projet
+            </figcaption>
+          </figure>
+          <figure class="max-w-lg mt-10">
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="/img/schema_materiel.png"
+              alt="image description"
+            />
+            <figcaption
+              class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400"
+            >
+              Schéma matériel
+            </figcaption>
+          </figure>
         </div>
       </div>
     </section>
